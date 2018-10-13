ceafc93a18f4f116c6f292b101863eaa76ada1c0
Ashutosh Srivastava <ashutosh@Ashutoshs-Mac-mini.local>
2018-10-13 18:51:24 +0530

    first commit

---
+++ src/test/java/com/oshitrundeck/oshitrundeck/OshitrundeckApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oshitrundeck.oshitrundeck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OshitrundeckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
